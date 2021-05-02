c742e665a7f6fe0a2aeca9352df0f81dcd368e77
amitnitjsr <amitnips007@gmail.com>
2021-05-03 00:57:06 +0530

    added ui and api

--- config/db.js
+++ config/db.js
@@ -7,7 +7,9 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
-            useNewUrlParser: true
+            useNewUrlParser: true,
+            userCreateIndex: true,
+            useFindAndModify: false,
         });
         console.log('MongoDB connected...')
     }
