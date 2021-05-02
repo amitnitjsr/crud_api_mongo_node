40eda6014538a662999dbce910f45ec942c619da
amitnitjsr <amitnips007@gmail.com>
2021-05-02 13:28:48 +0530

    user add

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const connectDB = require('./config/db');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const auth = require('./routes/api/auth');
 
 const app = express();
 
@@ -11,15 +13,20 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+connectDB();
+// mongoose
+//   .connect(db)
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err));
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('Hello World'));
 
 // Use Routes
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
