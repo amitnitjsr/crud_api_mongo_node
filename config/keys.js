bc71b907bcc6a6a4a92bc90077258368f040803e
amitnitjsr <amitnips007@gmail.com>
2021-05-02 10:52:36 +0530

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // mongoURI: 'mongodb://brad:brad@ds231725.mlab.com:31725/devconnector'
+  mongoURI: 'mongodb+srv://rest_api:99jyzz5eDv5T4CXa@contactkeeper.kszpp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
